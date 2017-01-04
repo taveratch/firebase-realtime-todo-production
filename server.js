f7cbe2b112f2582d7ce7cac6142dee77c0197b10
taweesoft <taweesoft@gmail.com>
2017-01-04 12:21:43 +0700

    fix server

--- server.js
+++ server.js
@@ -3,4 +3,7 @@ const path = require('path');
 const port = (process.env.PORT || 8080);
 let app = express()
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 app.listen(port)
